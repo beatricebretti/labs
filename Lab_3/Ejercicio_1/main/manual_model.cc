1b4214961c125dce0800af8d010f87b777c6c0c8
beatricebretti <bavaldes1@miuandes.cl>
2026-06-05 11:37:29 -0400

    add code

---
+++ Lab_3/Ejercicio_1/main/manual_model.cc
@@ -0,0 +1,6 @@
+#include "manual_model.h"
+#include <math.h>
+
+float manual_inference(float x) {
+    return sinf(x);
+}
---
+++ Lab_3/Ejercicio_1/main/manual_model.h
@@ -0,0 +1,6 @@
+#ifndef MANUAL_MODEL_H_
+#define MANUAL_MODEL_H_
+
+float manual_inference(float x);
+
+#endif
